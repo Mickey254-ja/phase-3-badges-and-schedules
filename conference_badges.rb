0d9ee5c62eb134c483b28a681f1166b654c639ca
Mickey254-ja <mickey.awino@student.moringaschool.com>
2023-02-08 15:04:50 +0300

    completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+require 'pry'
+def badge_maker (name = "Arel")
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_arr)
+    name_arr.map {|name| badge_maker(name)}
+end
+binding.pry
